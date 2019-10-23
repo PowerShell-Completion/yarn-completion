cc3d4237d5dd5c3489d3f15efca816774f5c56a4
Saran Tanpituckpong <sarunta@gmail.com>
2019-10-23 16:23:54 +0700

    :sparkles: Add Read-PackageJson & Get-PackageScripts

--- src/utils.ps1
+++ src/utils.ps1
@@ -9,3 +9,46 @@ function AreEqual($a, $b) {
 		return $a -eq $b
 	}
 }
+
+function Read-PackageJson {
+	begin {
+		$ErrorActionPreference = 'SilentlyContinue'
+	}
+
+	process {
+		$cwd = Get-Location
+		$packagePath = Join-Path $cwd 'package.json'
+
+		if (-not (Test-Path $packagePath -PathType Leaf)) {
+			return $null
+		}
+
+		$packagePath = Resolve-Path $packagePath
+
+		return Get-Content $packagePath | ConvertFrom-Json
+	}
+}
+
+function Get-PackageScripts {
+	begin {
+		$ErrorActionPreference = 'SilentlyContinue'
+	}
+
+	process {
+		$package = Read-PackageJson
+
+		if ((-not $package) -or (-not $package.scripts)) {
+			return $()
+		}
+
+		$scripts = $package.scripts | Get-Member -MemberType NoteProperty | ForEach-Object {
+			$_.Name
+		}
+
+		if (-not $scripts) {
+			return $()
+		}
+
+		return $scripts
+	}
+}
